9bed1a8d3d3f15103fe0b7ecbfba49d3b4cf3891
Joao Matheus <jmatheus_a@outlook.com.br>
2019-06-28 09:35:23 -0300

    Novo teste de erro

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,12 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
+		int a = 0;
 		System.out.println("Bom dia!");
 		System.out.println("Boa tarde!");
 		System.out.println("Boa noite!");
-
+		
+		System.out.println("Teste de erro" + a);
 	}
 
 }
